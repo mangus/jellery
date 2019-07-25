b412abe4c76e8763312023715db323a604d9218a
Mart Mangus <mart@mangus.ee>
2019-07-25 11:03:44 +0300

    Started adding swiping

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -41,6 +41,15 @@ function showNextSlide() {
           preloadImage(e.firstChild.src);
           exitLoop = true;
         } else if (e.classList.contains("next-slide")) {
+        
+          // TODO+TEST
+          var swiper = new Swipe(e.firstChild);
+          swiper.onLeft(function() { alert('swiped left.') });
+          swiper.onRight(function() { alert('swiped right.') });
+          swiper.onUp(function() { alert('swiped up.') });
+          swiper.onDown(function() { alert('swiped down.') });
+          swiper.run();
+          
           e.classList.add("slide-in");
           e.classList.remove("next-slide");
           changeNext = true;
@@ -65,4 +74,44 @@ function preloadImage(url, callback) {
 function showPreviousSlide() {
 }
 
+class Swipe {
+  constructor(element) {
+    this.xDown = null; this.yDown = null;
+    this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
+      this.element.addEventListener('touchstart', function(evt) {
+          this.xDown = evt.touches[0].clientX;
+          this.yDown = evt.touches[0].clientY;
+      }.bind(this), false);
+  }
+  
+  onLeft(callback) { this.onLeft = callback; return this; }
+  onRight(callback) { this.onRight = callback; return this; }
+  onUp(callback) { this.onUp = callback; return this; }
+  onDown(callback) { this.onDown = callback; return this; }
 
+  handleTouchMove(evt) {
+      if ( ! this.xDown || ! this.yDown ) { return; }
+      var xUp = evt.touches[0].clientX; var yUp = evt.touches[0].clientY;
+      this.xDiff = this.xDown - xUp; this.yDiff = this.yDown - yUp;
+      if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) {
+          if ( this.xDiff > 0 ) {
+              this.onLeft();
+          } else {
+              this.onRight();
+          }
+      } else {
+          if ( this.yDiff > 0 ) {
+              this.onUp();
+          } else {
+              this.onDown();
+          }
+      }
+      this.xDown = null; this.yDown = null;
+  }
+
+  run() {
+      this.element.addEventListener('touchmove', function(evt) {
+          this.handleTouchMove(evt).bind(this);
+      }.bind(this), false);
+  }
+}
